2b4d4aee14bd30bbc0a6900efb3bd22ee1e23fbf
Tim Boden <mail@timboden.de>
2020-10-18 23:38:57 +0200

    finalize sdl

--- externals/https/www.libsdl.org/tiki.lua
+++ externals/https/www.libsdl.org/tiki.lua
@@ -38,6 +38,7 @@ sdl_project.module.module_type = ModuleTypes.FilesModule
 
 sdl_project:add_include_dir( version_name .. "/include" )
 
+sdl_project:add_files( version_name .. "/include/*.h" )
 sdl_project:add_files( version_name .. "/src/*.h" )
 sdl_project:add_files( version_name .. "/src/*.c" )
 
@@ -46,10 +47,14 @@ sdl_modules = {
 	audio		= { header = true,	source = true,	platforms = {} },
 	core		= { header = false,	source = false,	platforms = {} },
 	cpuinfo		= { header = false,	source = true,	platforms = {} },
+	dynapi		= { header = true,	source = true,	platforms = {} },
 	events		= { header = true,	source = true,	platforms = {} },
+	file		= { header = false,	source = true,	platforms = {} },
 	filesystem	= { header = false,	source = false,	platforms = {} },
 	haptic		= { header = true,	source = true,	platforms = {} },
+	hidapi		= { header = false,	source = false,	platforms = {} },
 	joystick	= { header = true,	source = true,	platforms = {} },
+	libm		= { header = true,	source = true,	platforms = {} },
 	loadso		= { header = false,	source = false,	platforms = {} },
 	main		= { header = false,	source = false,	platforms = {} },
 	power		= { header = true,	source = true,	platforms = {} },
@@ -61,8 +66,20 @@ sdl_modules = {
 	video		= { header = true,	source = true,	platforms = {} }
 }
 
-sdl_modules[ "audio" ].platforms[ Platforms.Windows ]	= { wasapi	= { header = false,	source = true } }
-sdl_modules[ "main" ].platforms[ Platforms.Windows ]	= { windows	= { header = false,	source = true } }
+sdl_modules[ "audio" ].platforms[ Platforms.Windows ]		= { directsound = { header = true, source = true }, disk = { header = true, source = true }, dummy = { header = true, source = true }, wasapi = { header = false, source = true }, winmm = { header = true, source = true } }
+sdl_modules[ "core" ].platforms[ Platforms.Windows ]		= { windows	= { header = true,	source = true } }
+sdl_modules[ "filesystem" ].platforms[ Platforms.Windows ]	= { windows	= { header = false,	source = true } }
+sdl_modules[ "haptic" ].platforms[ Platforms.Windows ]		= { windows	= { header = true,	source = true } }
+sdl_modules[ "hidapi" ].platforms[ Platforms.Windows ]		= { windows	= { header = false,	source = true } }
+sdl_modules[ "joystick" ].platforms[ Platforms.Windows ]	= { windows	= { header = true,	source = true }, hidapi = { header = true, source = true } }
+sdl_modules[ "loadso" ].platforms[ Platforms.Windows ]		= { windows	= { header = false,	source = true } }
+sdl_modules[ "main" ].platforms[ Platforms.Windows ]		= { windows	= { header = false,	source = true } }
+sdl_modules[ "power" ].platforms[ Platforms.Windows ]		= { windows	= { header = false,	source = true } }
+sdl_modules[ "render" ].platforms[ Platforms.Windows ]		= { direct3d = { header = true, source = true }, opengl = { header = true, source = true }, opengles2 = { header = true, source = true }, software = { header = true, source = true } }
+sdl_modules[ "sensor" ].platforms[ Platforms.Windows ]		= { dummy	= { header = true,	source = true } }
+sdl_modules[ "thread" ].platforms[ Platforms.Windows ]		= { windows	= { header = true,	source = true }, generic = { header = true, source = true } }
+sdl_modules[ "timer" ].platforms[ Platforms.Windows ]		= { windows	= { header = false,	source = true } }
+sdl_modules[ "video" ].platforms[ Platforms.Windows ]		= { windows	= { header = true,	source = true }, dummy = { header = true, source = true }, yuv2rgb = { header = true, source = true } }
 
 for module_name, module_data in pairs( sdl_modules ) do
 	local module_path = version_name .. "/src/" .. module_name
@@ -94,7 +111,14 @@ end
 module:add_include_dir( version_name .. "/include" )
 
 module.import_func = function( project, solution )
-	throw( "test" )
 	project:add_project_dependency( sdl_project )
+	
+	if tiki.platform == Platforms.Windows then
+		project:add_library_file( "imm32" )
+		project:add_library_file( "winmm" )
+		project:add_library_file( "setupapi" )
+		project:add_library_file( "version" )
+	end
+	
 	solution:add_project( sdl_project )
 end
